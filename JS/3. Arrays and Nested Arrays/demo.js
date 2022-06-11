f8e1a2f5e708b14cd77f0110ab802665253cd386
Manchootop <96022434+Manchootop@users.noreply.github.com>
2022-06-11 13:04:24 +0300

    Add files via upload

--- JS/3. Arrays and Nested Arrays/demo.js
+++ JS/3. Arrays and Nested Arrays/demo.js
@@ -1,7 +1,28 @@
+// const myArr = [10, 20, 30, 40, 50];
+
+// console.log(myArr);
+
+// for (let n of myArr){
+//     console.log(n);
+// }
+
 const myArr = [10, 20, 30, 40, 50];
 
-console.log(myArr);
 
-for (let n of myArr){
-    console.log(n);
-}
+myArr.forEach(console.log)
+// const a = myArr[0];
+// const b = myArr[1];
+// console.log(a);
+// console.log(b);
+
+// const [c, d, ...params] = myArr;
+
+// console.log(...params);
+
+// shift 
+// unshift
+// pop
+// push
+
+// console.log(myArr.splice(1, 0, ...[21, 22, 23]));
+// console.log(myArr);
---
+++ JS/3. Arrays and Nested Arrays/firstLast.js
@@ -0,0 +1,12 @@
+function firstLast(numsAsStrings){
+    let first = [...numsAsStrings].shift();
+    let last = [...numsAsStrings].pop();
+
+    
+    const result = Number(first) + Number(last);
+    console.log(result);
+}
+
+firstLast(['20', '30', '40']);
+
+firstLast(['10']);
