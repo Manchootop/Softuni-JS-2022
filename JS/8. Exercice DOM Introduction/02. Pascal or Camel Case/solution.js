750127afdc2ad6452785cbe66f7261699e3b9b65
Manchootop <96022434+Manchootop@users.noreply.github.com>
2022-06-15 20:41:42 +0300

    Added solved problems

---
+++ JS/8. Exercice DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,31 @@
+function solve() {
+  let text = document.getElementById('text').value;
+  let conventionCase = document.getElementById('naming-convention').value;
+
+
+  let result = '';
+
+  let words = text.split(' ').filter(x => x != '');
+
+  if (conventionCase == 'Camel Case'){
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].toLowerCase();
+      if (i != 0){
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
+      }
+    }
+  } else if (conventionCase == 'Pascal Case'){
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].toLowerCase();
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
+
+    }
+  } else {
+    return document.getElementById('result').textContent = 'Error!';
+  }
+
+  result = words.join('')
+  
+  document.getElementById('result').textContent = result;
+  console.log(words);
+}
